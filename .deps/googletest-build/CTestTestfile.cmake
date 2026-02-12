57c11955c61b7c3a9742646534b12a4adacfcae1
Jiang Kai <181953023@qq.com>
2026-02-12 11:11:13 +0800

    chore: 清理构建生成的googletest依赖文件
    移除CMake构建过程中生成的googletest依赖目录和临时文件，这些文件在构建完成后不再需要，清理以保持项目目录整洁。

--- .deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: H:/Resources/RTLinux/Demos/MB_DDF/.deps/googletest-src
-# Build directory: H:/Resources/RTLinux/Demos/MB_DDF/.deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- .deps/googletest-build/googlemock/CMakeFiles/gmock.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "H:/Resources/RTLinux/Demos/MB_DDF/build/aarch64/Debug/lib/libgmock.a"
-)
